000818691f47179667bae4d11626f7deca72fa08
Teo Welton <teowelton@gmail.com>
2024-08-18 18:56:31 -0600

    SQLite library database + site rewrite

--- src/database.rs
+++ src/database.rs
@@ -1,6 +1,45 @@
-use log::trace;
-use rusqlite::Connection;
+use log::{debug, trace};
+use sqlite::Connection;
 
 pub struct Database {
     conn: Connection,
+}
+
+impl Database {
+    pub fn new(db_name: &str) -> Self {
+        let conn = sqlite::open(format!("{}.db", db_name)).unwrap();
+        return Database { conn };
+    }
+
+    pub fn setup(&self) {
+        trace!("Setting Up SQLite Table");
+
+        self.conn
+            .execute(
+                r#"
+              CREATE TABLE IF NOT EXISTS sites (
+                id TEXT PRIMARY KEY,
+                url TEXT NOT NULL,
+                crawl_time TEXT NOT NULL,
+                links_to TEXT
+              );"#,
+            )
+            .unwrap();
+    }
+
+    pub fn prepare(&self, statement: &str) -> sqlite::Result<sqlite::Statement<'_>> {
+        trace!("Preparing SQLite Statement: '{}'", statement);
+
+        return self.conn.prepare(statement);
+    }
+
+    pub fn execute(&self, statement: &str) -> sqlite::Result<()> {
+        trace!("Executing SQLite Statement: '{}'", statement);
+
+        return self.conn.execute(statement);
+    }
+
+    // TODO: Function to retrieve all site Uuids
+    // TODO: Function to read `Site` by id
+    // TODO: Function to write `Site`
 }
--- src/site.rs
+++ src/site.rs
@@ -16,4 +16,58 @@ impl std::fmt::Display for Site {
     }
 }
 
-impl Site {}
+impl Site {
+    fn read_into(id: Uuid, database: &Database) -> Option<Self> {
+        let query = format!(
+            "SELECT url, crawl_time, links_to FROM sites WHERE id = '{}'",
+            id
+        );
+
+        let mut statement = database.prepare(&query).unwrap();
+
+        while let sqlite::State::Row = statement.next().unwrap() {
+            let url: String = statement.read::<String, usize>(0).unwrap();
+            let crawl_time_str: String = statement.read::<String, usize>(1).unwrap();
+            let links_to_str: String = statement.read::<String, usize>(2).unwrap();
+
+            let crawl_time = DateTime::parse_from_rfc3339(&crawl_time_str)
+                .unwrap()
+                .with_timezone(&Utc);
+
+            let links_to = match links_to_str.is_empty() {
+                true => vec![],
+                false => links_to_str
+                    .split(',')
+                    .map(|s| Uuid::from_str(s).unwrap())
+                    .collect(),
+            };
+
+            return Some(Self {
+                id: id.clone(),
+                url,
+                crawl_time,
+                links_to,
+            });
+        }
+
+        return None;
+    }
+
+    fn write_into(&self, database: &Database) {
+        let links_to_str = self
+            .links_to
+            .iter()
+            .map(|uuid| uuid.to_string())
+            .collect::<Vec<String>>()
+            .join(",");
+
+        let crawl_time_str = self.crawl_time.to_rfc3339();
+
+        let query = format!(
+            "INSERT OR REPLACE INTO sites (id, url, crawl_time, links_to) VALUES ('{}', '{}', '{}', '{}')",
+            self.id, self.url, crawl_time_str, links_to_str
+        );
+
+        database.execute(&query).unwrap();
+    }
+}
